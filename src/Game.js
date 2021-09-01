a30d69221f2d79a0d00a4a9d5ceeaeef4496ff07
rblinde <robertblinde@gmail.com>
2021-09-01 14:30:25 +0200

    Players can place pieces in order

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,11 @@
 class Game {
   constructor() {
     this.boardElem = document.getElementById('board');
-    this.selected = null;
+    this.pieceElems = [...document.querySelectorAll('.pieces input[type="radio"]')];
+    this.tileElems = [...this.boardElem.children];
+    this.board = Array(9).fill(0);
+    this.currentPlayer = 'red';
+    this.selectedPiece = null;
 
     this.addEventListeners();
     this.handleWindowResize();
@@ -13,6 +17,8 @@ class Game {
    */
   addEventListeners() {
     window.addEventListener('resize', () => this.handleWindowResize(), false);
+    this.pieceElems.forEach(piece => piece.addEventListener('click', (e) => this.handlePieceClick(e), false));
+    this.tileElems.forEach(tile => tile.addEventListener('click', (e) => this.handleTileClick(e), true));
   }
 
 
@@ -29,10 +35,69 @@ class Game {
 
 
   /**
+   * Sets selected piece if color matches current player
+   * @param {Event} e
+   */
+  handlePieceClick(e) {
+    const color = e.target.parentNode.parentNode.dataset.color;
+
+    if (color !== this.currentPlayer) {
+      e.target.checked = false;
+      return false;
+    }
+
+    this.selectedPiece = e.target;
+  }
+
+
+  /**
+   * If possible, places piece of current player in selected tile
+   * @param {Event} e
+   */
+  handleTileClick(e) {
+    if (!this.selectedPiece) {
+      return false;
+    }
+
+    const tileElem = e.target.closest('.tile');
+    const index = this.tileElems.indexOf(tileElem);
+    const size = parseInt(this.selectedPiece.value);
+
+    if (this.board[index].size >= size) {
+      return false;
+    }
+
+    // Update board
+    this.updateTile(index, size);
+    this.board[index] = { size, player: this.currentPlayer };
+
+    // Switch player and reset piece
+    this.currentPlayer = this.currentPlayer === 'red' ? 'blue' : 'red';
+    this.selectedPiece.parentNode.classList.add('hide');
+    this.selectedPiece = null;
+  }
+
+
+  /**
+   * Updates DOM element of tile via `index`
+   * @param {Number} index
+   * @param {Number} size
+   */
+  updateTile(index, size) {
+    const piece = document.createElement('div');
+    piece.classList.add('ppiece', `ppiece--${size}`, `ppiece--${this.currentPlayer}`);
+    this.tileElems[index].textContent = null;
+    this.tileElems[index].appendChild(piece);
+  }
+
+
+  /**
    * Starts a game of trick-tac-toe
    */
   start() {
-    // TODO
+    for (const piece of this.pieceElems) {
+      piece.checked = false;
+    }
   }
 }
 
